2e55bc18b238ed4c2239ed4cade598409e55b7ae
Asifmikdad <asifmikdad@gmail.com>
2022-08-14 01:48:04 +0600

    update

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -3,12 +3,20 @@ import React from 'react';
 const Service = ({ service }) => {
     const {name, slots} = service;
     return (
-        <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
             <div class="card-body">
                 <h2 class="card-title">{name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                <p>
+                    {
+                        slots.length > 0
+                        ? <span>{slots[0]}</span>
+                        :<span>Try another day</span>
+                    }
+                </p>
+
+                <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} Available</p>
+                <div class="card-actions justify-center">
+                    <button disabled={slots.length===0} class="btn btn-primary text-white uppercase">Book Appointments</button>
                 </div>
             </div>
         </div>
