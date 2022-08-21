3a1a3ae154681000af0faad73dc6d082d57b4076
Asifmikdad <asifmikdad@gmail.com>
2022-08-22 00:02:43 +0600

    update

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { format } from 'date-fns';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { toast } from 'react-toastify';
 
 const BookingModal = ({ date, treatment, setTreatment }) => {
     const { _id, name, slots } = treatment;
@@ -22,9 +23,20 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
 
         }
 
+        fetch('http://localhost:5000/booking', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setTreatment(null);
+            })
 
 
-        setTreatment(null);
     }
 
     return (
